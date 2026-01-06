ffea013df878c5d88577b0dda47004f720918387
wuwla06 <timur.rybin2006@icloud.com>
2026-01-06 14:50:05 +0300

    Add files via upload

---
+++ modeling-2/constants.py
@@ -0,0 +1,20 @@
+K = 1.0
+
+CHARGES = [
+    (-2.0, 0.0, 3.0),   
+    (2.0, 0.0, -3.0), 
+    (0.0, 2.0, 2.0),    
+    (0.0, -2.0, -2.0), 
+]
+
+AUTO_BOUNDS_MARGIN = 2.0
+
+LINE_STEP = 0.03          
+LINE_LENGTH = 2000        
+MIN_LINE_LENGTH = 20      
+
+LINES_PER_CHARGE = 24   
+START_RADIUS_FACTOR = 0.15 
+
+RESULTS_DIR = "results"
+RESULT_FILENAME = "electric_field_result.png"
